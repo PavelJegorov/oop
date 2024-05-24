d5dede3cfa6ad8e300b94600d72df2ffa20f21ee
opilane <opilane@HP-CZC2349HTF>
2024-05-24 13:12:42 +0300

    Add project files.

---
+++ oop/oop.py
@@ -0,0 +1,29 @@
+
+#class student:
+#    def hello(self):
+#        print("hello",self.name)
+
+#    def greetFriends(self, name):
+#        print(f"hello {name}")
+
+
+#    def __init__(self, name, title, age):
+#        print("Initializing student..")
+#        self.name = name
+#        self.title = title
+#        self.age = age
+
+#    def hello(self):
+#        print(self.name)
+
+#    def upgrade(self):
+#        self.title = "!"
+#        self.age += 1
+
+#S = student("Pavel","Sir",17)
+#S.hello()
+#print(S.age)
+
+
+
+
